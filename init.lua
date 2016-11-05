ffb24d285f192c03af1049018fd2ba522de307cb
Rui <rui.minetest@gmail.com>
2016-11-05 15:17:08 +0900

    Simplify checking depend

--- init.lua
+++ init.lua
@@ -1,46 +1,62 @@
 carpets = {}
 
-dofile(minetest.get_modpath("carpets").."/carpet_api.lua")
-dofile(minetest.get_modpath("carpets").."/modutils.lua")
+local modname = minetest.get_current_modname()
+local modpath = minetest.get_modpath(modname)
 
-depmod = carpets.modutils.get_depmod("carpets")
+dofile(modpath .. "/api.lua")
 
+local depends = (function()
+	local file = io.open(modpath .. "/depends.txt")
+	if not file then
+		return
+	end
 
-local function enabledfilter(name, def)
--- disable carpets from loaded modules but not defined in dependency
-	if depmod:check_depmod(name) == false then
+	local depends = {}
+	for line in file:lines() do
+		if line:sub(-1) == "?" then
+			line = line:sub(1, -2)
+		end
+		depends[line] = true
+	end
+
+	file:close()
+
+	return depends
+end)()
+
+local function filter(name, def)
+	-- disable carpets from loaded modules but not defined in dependency
+	if not depends[def.mod_origin] then
 		return false
 	end
 
--- disable carpets for blocks without description
+	-- disable carpets for blocks without description
 	if def.description == nil or def.description == "" then
 		return false
 	end
 
--- not supported node types for carpets
-	if def.drawtype == "liquid"     or
-	   def.drawtype == "firelike"   or
-	   def.drawtype == "airlike"    or
-	   def.drawtype == "plantlike"  or
-	   def.drawtype == "nodebox"  or
-	   def.drawtype == "raillike"   then
+	-- not supported node types for carpets
+	if def.drawtype == "liquid"    or
+	   def.drawtype == "firelike"  or
+	   def.drawtype == "airlike"   or
+	   def.drawtype == "plantlike" or
+	   def.drawtype == "nodebox"   or
+	   def.drawtype == "raillike"  then
 		return false
 	end
 
--- no carpet for signs, rail, ladder
+	-- no carpet for signs, rail, ladder
 	if def.paramtype2 == "wallmounted" then
 		return false
 	end
 
--- all checks passed
+	-- all checks passed
 	return true
 end
 
-
-------------------------------------------------
 -- main execution
 for name, def in pairs(minetest.registered_nodes) do
-	if enabledfilter(name, def) then
+	if filter(name, def) then
 		carpets.register(name)
 	end
 end
